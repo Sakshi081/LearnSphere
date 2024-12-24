15d70fd814c132d9da7eac61222a6de445c5b4e7
Sakshi Goyal <98842480+Sakshi081@users.noreply.github.com>
2024-12-24 21:46:09 +0530

    Update register.js

--- register.js
+++ register.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA3HwfpVzegW1jGXyoH_TQ6JXmRdD7puaE",
-  authDomain: "learnsphere-a1c95.firebaseapp.com",
-  projectId: "learnsphere-a1c95",
-  storageBucket: "learnsphere-a1c95.firebasestorage.app",
-  messagingSenderId: "652736746414",
-  appId: "1:652736746414:web:273c100900291e1696ae3f",
-  measurementId: "G-457Z527KH9"
+  apiKey: "xxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxx"
 };
 
 // Initialize Firebase
